4211e310420855c8ea0c8c3c48578731f8f2f2a8
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-04-08 23:53:30 -0400

    Apply layout for currency converter screen

--- LOTR Converter/LOTR Converter/ContentView.swift
+++ LOTR Converter/LOTR Converter/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                HStack {
+                    VStack {
+                        HStack {
+                            Image(.silverpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            Text("Silver piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        // Textfield
+                        Text("Textfield")
+                    }
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    VStack {
+                        HStack {
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        
+                        Text("Textfield")
+                    }
+                }
+                Spacer()
+                // Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
     }
 }
 
