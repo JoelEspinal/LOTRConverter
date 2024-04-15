b678a39e7aff1093e6f9c2ae8c3900e429bd48bb
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-04-14 22:10:05 -0400

    Add exchange view

---
+++ LOTR Converter/LOTR Converter/ExcangeInfo.swift
@@ -0,0 +1,60 @@
+//
+//  ExcangeInfo.swift
+//  LOTR Converter
+//
+//  Created by Joel Espinal on 14/4/24.
+//
+
+import SwiftUI
+
+struct ExcangeInfo: View {
+    var body: some View {
+        ZStack {
+            // backround
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            VStack {
+                // title text
+                Text("Exchange rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                // description text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title2)
+                    .padding()
+                // exchange rates
+                HStack {
+                    // left currency image
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    // exchange rate text
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    // right currency image
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                }
+                
+                // done button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+            }
+            .foregroundStyle(.black)
+        }
+    }
+}
+
+#Preview {
+    ExcangeInfo()
+}
