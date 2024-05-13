d38a85bcaf33b7f3ed00f0ff612a880d38907f5d
JoelEspinal <joel_espinal@claro.com.do>
2024-05-13 09:34:39 -0400

    Add focus state wrapper

--- LOTR Converter/LOTR Converter/ContentView.swift
+++ LOTR Converter/LOTR Converter/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State var leftAmount = ""
     @State var rightAmount = ""
     
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
     @State var leftCurrency: Currency = .silverPiece
     @State var rightCurrency: Currency = .goldPiece
     
@@ -50,9 +53,12 @@ struct ContentView: View {
                         // Textfield
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
+                            .focused($leftTyping)
                             .onChange(of: leftAmount) {
-                                rightAmount = leftCurrency
-                                    .convert(leftAmount, to: rightCurrency)
+                                if leftTyping {
+                                    rightAmount = leftCurrency
+                                        .convert(leftAmount, to: rightCurrency)
+                                }
                             }
                     }
                     Image(systemName: "equal")
@@ -78,6 +84,12 @@ struct ContentView: View {
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
+                            .focused($rightTyping)
+                            .onChange(of: rightAmount, {
+                                if rightTyping {
+                                    leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                                }
+                            })
                     }
                 }
                 .padding()
